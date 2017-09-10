b72042ce789552f3c405f4ef710791bb09b86a03
Robert Laws <roblaws@me.com>
2017-09-10 10:08:14 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,4 +5,13 @@ class Doctor
     @name = name
     @appointments = []
   end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
+
+  def appointments
+    @appointments
+  end
 end
