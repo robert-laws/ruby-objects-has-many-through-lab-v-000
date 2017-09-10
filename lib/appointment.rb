8705b70a2ccaccc982534321ffc9a1d76382e4e8
Robert Laws <roblaws@me.com>
2017-09-10 10:11:06 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,7 +1,12 @@
 class Appointment
-  attr_accessor :name
+  attr_accessor :name. :doctor
 
-  def initializes(name)
+  def initializes(name, doctor)
     @name = name
+    @doctor = doctor
+  end
+
+  def patient
+
   end
 end
