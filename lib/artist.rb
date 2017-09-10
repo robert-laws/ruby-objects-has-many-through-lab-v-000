176b06cf4dfda4d3ad6c8492cd9f2520acda14bf
Robert Laws <roblaws@me.com>
2017-09-10 09:27:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,13 @@
 class Artist
   attr_accessor :name
+  attr_reader :songs
 
   def initialize(name)
     @name = name
     @songs = []
+  end
+
+  def add_song(song, genre)
+    @songs << song
   end
 end
